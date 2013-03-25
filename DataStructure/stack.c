6f6409e5573e661724474f54aea4a14013e5aeea
ywllx <yanwanglong@gmail.com>
2013-03-26 02:01:13 +0800

    rename directory

---
+++ DataStructure/stack.c
@@ -0,0 +1,19 @@
+/* stack.c */
+#include <stdio.h>
+
+#include "list.h"
+#include "stack.h"
+
+/* stack push */
+int stack_push(Stack *stack, const void *data)
+{
+	/* push the data onto the stack like insert the data to the head of the list */
+	return list_ins_next(stack, NULL, data);
+}
+
+/* stack pop */
+int stack_pop(Stack *stack, void **data)
+{
+	/* pop the data of the stack like remove the data of the head of the list */
+	return list_rem_next(stack, NULL, data);
+}
